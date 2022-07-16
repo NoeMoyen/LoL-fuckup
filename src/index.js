37691fc4162b63f24eca02a92319ddf6c820700e
NoeMoyen <noe.moyen@epitech.eu>
2022-07-16 10:15:37 +0200

    [add] get clean connection info via a connector

--- src/index.js
+++ src/index.js
@@ -1,49 +1,15 @@
-const { ftruncateSync } = require('fs');
-const fetch = require('node-fetch');
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const LCUConnector = require('./lib/LCUConnector.js');
 
-const Secret = require('./secrets');
+const connector = new LCUConnector();
 
-const btoa = (string) => {
-    return Buffer.from(string).toString('base64');
-}
+connector.on('connect', data => {
+    console.log('League Client has started', data);
+});
 
-const auth = `${btoa(`${Secret.username}:${Secret.password}`)}`;
+connector.on('disconnect', () => {
+    console.log('League Client has been closed');
+});
 
-const get = (endpoint) => {
-    console.log('starting get %s', endpoint);
-    return fetch(`https://127.0.0.1:${Secret.port}${endpoint}`, {
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Basic ${auth}`
-        }
-    })
-    .then(res => res.json())
-    .then(res => {
-        console.log('GET request finish for: %s', endpoint);
-        return res;
-    });
-}
-
-const isFriendOffline = (friend) => friend.availability === 'offline';
-
-const serial = funcs => funcs.reduce((promiseChain, currentTask) =>
-    promiseChain.then(chainResults =>
-        currentTask().then(currentResult =>
-            [...chainResults, currentResult]
-        )
-    ),
-    Promise.resolve([])
-);
-
-(async function main() {
-    const friends = await get(`/lol-chat/v1/friends`);
-    const offlineFriends = friends.filter(isFriendOffline);
-
-    const summonerFetchers = offlineFriends.slice(0,10).map(friend => () => get(`/lol-summoner/v1/summoners/${friend.summonerId}`));
-
-    const results = await serial(summonerFetchers);
-
-    console.log(results);
-
-})();
+// Start listening for the LCU client
+connector.start();
+console.log('Listening for League Client');
--- src/secrets.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    pid: 26896,
-    host: '127.0.0.1',
-    port: 50540,
-    username: 'riot',
-    password: 'DDXD-uKR_sBchbJtSVM7kQ',
-};
